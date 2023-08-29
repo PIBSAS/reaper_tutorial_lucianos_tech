c763725d131eb9a00e08ed5eb100f96aba78ae7c
Raspberry Pi Buenos Aires <38385411+Luciano2018@users.noreply.github.com>
2023-08-29 14:24:28 -0300

    Update rpibsas_reaper.sh

--- rpibsas_reaper.sh
+++ rpibsas_reaper.sh
@@ -77,7 +77,7 @@ mkdir Music
 wget -c https://archive.org/download/Samplicity/Samplicity_M7_Main-02-Wave32bit-48Khz_v1.1.zip
 unzip Sampli*.zip
 rm Sampli*.zip
-mv Samplicity_M7_Main-02-Wave32bit-48Khz_v1.1 Music/
+mv "Samplicity M7 Main - 02 - Wave, 32 bit, 48 Khz, v1.1"/ Music/
 echo
 echo
 echo "Now depending on your OS architecture will install the rest"
