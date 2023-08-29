b7784f0c5ee1231ac64cacdf7b7352e5d3d60e2c
Raspberry Pi Buenos Aires <38385411+Luciano2018@users.noreply.github.com>
2023-08-29 16:23:34 -0300

    Update rpibsas_reaper.sh
    Beauty and fix Samplicity typo

--- rpibsas_reaper.sh
+++ rpibsas_reaper.sh
@@ -5,6 +5,7 @@
 echo
 echo "Start Reaper Tutorial Install from Raspberry Pi Buenos Aires this will take some time, so connect Ethernet Cable!"
 echo
+ARCH=64
 #Versions
 REAPER=682
 KX=11.1.0
@@ -14,10 +15,12 @@ CMAKE=3.27.4
 REAPACK=1.2.4.3
 DSP=1.2.30
 echo
-echo "First the common install for any architecture OS"
+echo "First the common install for Pi OS $ARCH bit"
 echo
-echo "Adding KXStudio Repository to the OS"
 echo
+echo "##########################################"
+echo "## Adding KXStudio Repository to the OS ##"
+echo "##########################################"
 cd
 wget -c https://launchpad.net/~kxstudio-debian/+archive/ubuntu/kxstudio/+files/kxstudio-repos_${KX}_all.deb
 sudo dpkg -i kxstudio*.deb
@@ -25,7 +28,10 @@ rm kxstudio-repos_${KX}_all.deb
 echo
 echo "KXStudio Repo Added"
 echo
-echo "Adding ZynthianOS Plugins"
+echo "##########################################"
+echo "####### Adding ZynthianOS Plugins ########"
+echo "##########################################"
+echo
 cd
 git clone https://github.com/zynthian/zynthian-plugins.git
 echo
@@ -36,7 +42,9 @@ rm -rf zynthian-plugins
 echo
 echo "ZynthianOS Plugins Installed"
 echo
-echo "Compile and install CMake requiered by Stochas"
+echo "##########################################################"
+echo "##### Compile and install CMake requiered by Stochas #####"
+echo "##########################################################"
 sudo apt update
 sudo apt install -y libssl-dev
 wget -c https://github.com/Kitware/CMake/releases/download/v${CMAKE}/cmake-${CMAKE}.tar.gz
@@ -53,7 +61,16 @@ rm $HOME/cmake*.gz
 echo
 echo "CMAKE compiled"
 echo
-echo "Adding Ninjas 2 Plugins"
+echo "###############################"
+echo "##### Ninjas 2 Standalone #####"
+echo "###############################"
+echo
+sudo apt install -y libjack-jackd2-dev
+echo
+echo
+echo "##########################################"
+echo "######## Compile Ninjas 2 Plugins ########"
+echo "##########################################"
 echo
 cd
 sudo apt install -y libgl1-mesa-dev libx11-dev libsndfile1-dev libsamplerate0-dev
@@ -67,22 +84,15 @@ echo
 sudo make install
 echo
 rm -rf ../ninjas2
+cd
 echo
 echo "Ninjas 2 Installed"
 echo
-cd
-echo
-echo "Getting Samplicity Samples and save to Music Folder I will make one folder if you are not  in English OS"
-mkdir Music
-wget -c https://archive.org/download/Samplicity/Samplicity_M7_Main-02-Wave32bit-48Khz_v1.1.zip
-unzip Sampli*.zip
-rm Sampli*.zip
-mv "Samplicity M7 Main - 02 - Wave, 32 bit, 48 Khz, v1.1"/ Music/
 echo
 echo
-echo "Now depending on your OS architecture will install the rest"
-echo
-ARCH=64
+echo "########################################"
+echo "##### Install Reaper DAW for armhf #####"
+echo "########################################"
 cd
 rm reaper*.tar.xz
 rm -rf reaper_linux_aarch64
@@ -95,10 +105,10 @@ echo
 cd reaper_linux_aarch64
 sudo ./install-reaper.sh --install /opt --integrate-desktop --usr-local-bin-symlink --quiet
 echo
-echo "REAPER Installed"
-echo
 rm ../reaper*.tar.xz
 rm -rf ../reaper_linux_aarch64
+echo
+echo "REAPER Installed"
 echo
 echo "Open then close Reaper to get the new Folders created in the first run"
 echo
@@ -106,13 +116,22 @@ reaper &
 sleep  10
 sudo pkill reaper
 echo
-echo "Install Plugins various"
+echo
+echo "#####################################"
+echo "########## Install Plugins ##########"
+echo "#####################################"
 echo
 sudo apt install -y vitalium zynaddsubfx zynaddsubfx-dssi zynaddsubfx-lv2 zynaddsubfx-vst wah-plugins swh-lv2 swankyamp swankyamp-lv2 swankyamp-vst sorcer yoshimi carla carla-lv2 helm airwindows cardinal cardinal-lv2 cardinal-vst2 cardinal-vst3 phasex lmms adlplug ams amsynth ardour drumkv1-lv2 fomp hydrogen padthv1-lv2 zam-plugins wolf-spectrum wolf-shaper teragonaudio-plugins temper shiro-plugins tal-plugins pizmidi-plugins pitcheddelay oxefmsynth obxd mda-lv2 luftikus lsp-plugins-vst lufsmeter linuxsampler-vst juced-plugins klangfalter juce-opl jackass hybridreverb2 easyssp drumgizmo drowaudio-plugins dpf-plugins distrho-plugin-ports dexed arctican-plugins vocproc tap-lv2 synthv1-lv2 zlfo so-synth-lv2 sherlock.lv2 samplv1-lv2 rubberband-lv2 noise-repellent moony.lv2 mod-pitchshifter mod-distortion melmatcheq.lv2 lv2vocoder kxstudio-recommended-audio-plugins-lv2 infamous-plugins gxvoxtonebender gxplugins guitarix-lv2 guitarix-ladspa geonkick eq10q drmr caps-lv2 calf-plugins bshapr bsequencer blop-lv2 bjumblr beatslash-lv2 avldrums.lv2 abgate fabla wolpertinger iem-plugin-suite-vst cv-lfo-blender-lv2
 echo
-echo "LSP Plugins"
 echo
-echo "VST2"
+echo "#################################"
+echo "########## LSP Plugins ##########"
+echo "#################################"
+echo
+echo "################"
+echo "##### VST2 #####"
+echo "################"
+echo
 cd
 wget -c https://sourceforge.net/projects/lsp-plugins/files/lsp-plugins/${LSP}/Linux-aarch64/lsp-plugins-vst2-${LSP}-Linux-aarch64.tar.gz
 echo
@@ -122,7 +141,11 @@ sudo cp -r usr/* /usr
 echo
 echo "VST2 Installed"
 echo
-echo "LV2"
+echo
+echo "###############"
+echo "##### LV2 #####"
+echo "###############"
+echo
 echo
 cd
 wget -c https://sourceforge.net/projects/lsp-plugins/files/lsp-plugins/${LSP}/Linux-aarch64/lsp-plugins-lv2-${LSP}-Linux-aarch64.tar.gz
@@ -133,7 +156,11 @@ sudo cp -r usr/* /usr
 echo
 echo "LV2 Installed"
 echo
-echo "JACK"
+echo
+echo "################"
+echo "##### JACK #####"
+echo "################"
+echo
 echo
 cd
 wget -c https://sourceforge.net/projects/lsp-plugins/files/lsp-plugins/${LSP}/Linux-aarch64/lsp-plugins-jack-${LSP}-Linux-aarch64.tar.gz
@@ -144,7 +171,11 @@ sudo cp -r usr/* /usr
 echo
 echo "JACK Installed"
 echo
-echo "LADSPA"
+echo
+echo "##################"
+echo "##### LADSPA #####"
+echo "##################"
+echo
 echo
 cd
 wget -c https://sourceforge.net/projects/lsp-plugins/files/lsp-plugins/${LSP}/Linux-aarch64/lsp-plugins-ladspa-${LSP}-Linux-aarch64.tar.gz
@@ -155,7 +186,11 @@ sudo cp -r usr/* /usr
 echo
 echo "LADSPA Installed"
 echo
-echo "CLAP"
+echo
+echo "################"
+echo "##### CLAP #####"
+echo "################"
+echo
 echo
 cd
 wget -c https://sourceforge.net/projects/lsp-plugins/files/lsp-plugins/${LSP}/Linux-aarch64/lsp-plugins-clap-${LSP}-Linux-aarch64.tar.gz
@@ -169,7 +204,13 @@ echo
 cd
 rm -rf lsp*
 echo
-echo "X42 Plugins"
+echo "LSP Installed"
+echo
+echo
+echo "#######################"
+echo "##### X42 Plugins #####"
+echo "#######################"
+echo
 echo
 cd
 wget -c http://x42-plugins.com/x42/linux/x42-compressor-v${X42}-arm64.tar.gz
@@ -183,7 +224,11 @@ echo "X42 Installed"
 echo
 cd
 echo
-echo "Compille and Install Stochas"
+echo
+echo "########################################"
+echo "##### Compille and Install Stochas #####"
+echo "########################################"
+echo
 echo
 cd
 sudo apt update
@@ -208,17 +253,21 @@ cp -rf $HOME/stochas/build/stochas_artefacts/VST3/Stochas.vst3 $HOME/.vst3
 rm -rf $HOME/stochas
 cd
 echo
-echo  "Ninjas 2 Standalone"
 echo
-sudo apt install -y libjack-jackd2-dev
+echo "###################"
+echo "##### ReaPack #####"
+echo "###################"
 echo
-echo "ReaPack"
 echo
 cd
 wget -c https://github.com/cfillion/reapack/releases/download/v${REAPACK}/reaper_reapack-aarch64.so
 mv reaper_reapack*.so $HOME/.config/REAPER/UserPlugins/
 echo
-echo "SWS S&M Extension"
+echo
+echo "#############################"
+echo "##### SWS S&M Extension #####"
+echo "#############################"
+echo
 echo
 cd $HOME/.config/REAPER/
 wget -c https://sws-extension.org/download/featured/sws-2.12.1.3-Linux-aarch64.tar.xz
@@ -226,7 +275,9 @@ tar -Jxvf sws*.tar.xz
 rm sws*
 cd
 echo
-echo "DSP53600 Access Virus C"
+echo "###################################"
+echo "##### DSP53600 Access Virus C #####"
+echo "###################################"
 cd
 wget -c https://futurenoize.com/dsp56300/builds/master/DSP56300Emu-${DSP}-Linux_aarch64-Osirus-VST2.deb
 wget -c https://futurenoize.com/dsp56300/builds/master/DSP56300Emu-${DSP}-Linux_aarch64-OsirusFX-VST2.deb
@@ -236,7 +287,13 @@ cd
 wget -c "https://archive.org/download/access-virus-b-c-roms/Access%20Virus%20C%20%28am29f040b_6v6%29.zip/Access%20Virus%20C%20%28am29f040b_6v6%29.BIN"
 sudo cp Access\ Virus\ C\ \(am29f040b_6v6\).BIN /usr/local/lib/vst/
 echo
-echo "Surge XT This will take a lot of time"
+echo "DSP53600 Emulator Installed"
+echo
+echo
+echo "#################################################"
+echo "##### Surge XT This will take a lot of time #####"
+echo "#################################################"
+echo
 cd
 git clone https://github.com/surge-synthesizer/surge.git
 cd surge
@@ -250,6 +307,18 @@ echo
 sudo cmake --install .
 echo
 echo "Surge XT Plugins installed"
+echo
+echo
+echo "####################################################################################################################"
+echo "##### Getting Samplicity Samples and save to Music Folder I will make one folder if you are not  in English OS #####"
+echo "####################################################################################################################"
+mkdir Music
+wget -c https://archive.org/download/Samplicity/Samplicity_M7_Main-02-Wave32bit-48Khz_v1.1.zip
+unzip Sampli*.zip
+rm Sampli*.zip
+mv "Samplicity M7 Main - 02 - Wave, 32 bit, 48 Khz, v1.1"/ Music/
+echo
+echo "Samples Downloaded"
 echo
 echo "Finished install on $ARCH bits OS, check tutorial for Tukan plugins"
 echo
