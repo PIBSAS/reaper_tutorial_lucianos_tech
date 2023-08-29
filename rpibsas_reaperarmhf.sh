b50b8e2c42b5917a537b533973a8ab053775ff72
Raspberry Pi Buenos Aires <38385411+Luciano2018@users.noreply.github.com>
2023-08-29 13:32:52 -0300

    Create rpibsas_reaperarmhf.sh

---
+++ rpibsas_reaperarmhf.sh
@@ -0,0 +1,287 @@
+#! /bin/bash
+#########################################################################################################################
+#################################/////RASPBERRY PI BUENOS AIRES/////#####################################################
+#########################################################################################################################
+echo
+echo "Start Reaper Tutorial Install from Raspberry Pi Buenos Aires this will take some time, so connect Ethernet Cable!"
+echo
+#Versions
+REAPER=682
+KX=11.1.0
+LSP=1.2.10
+X42=0.6.5
+CMAKE=3.27.4
+REAPACK=1.2.4.3
+DSP=1.2.30
+echo
+echo "First the common install for architecture OS"
+echo
+echo "##########################################"
+echo "## Adding KXStudio Repository to the OS ##"
+echo "##########################################"
+echo
+cd
+wget -c https://launchpad.net/~kxstudio-debian/+archive/ubuntu/kxstudio/+files/kxstudio-repos_${KX}_all.deb
+sudo dpkg -i kxstudio*.deb
+rm kxstudio-repos_${KX}_all.deb
+echo
+echo "KXStudio Repo Added"
+echo
+echo "##########################################"
+echo "####### Adding ZynthianOS Plugins ########"
+echo "##########################################"
+cd
+git clone https://github.com/zynthian/zynthian-plugins.git
+echo
+cd zynthian-plugins
+sudo cp -r * /usr/local/lib
+cd
+rm -rf zynthian-plugins
+echo
+echo "ZynthianOS Plugins Installed"
+echo
+echo "##########################################################"
+echo "##### Compile and install CMake requiered by Stochas #####"
+echo "##########################################################"
+sudo apt update
+sudo apt install -y libssl-dev
+wget -c https://github.com/Kitware/CMake/releases/download/v${CMAKE}/cmake-${CMAKE}.tar.gz
+tar -xf cmake*.tar.gz
+cd cmake*/
+./bootstrap
+gmake
+sudo make install
+echo
+rm -rf $HOME/cmake*/
+rm $HOME/cmake*.gz
+echo
+echo "CMake compiled"
+echo
+echo "#########################################"
+echo "######## Adding Ninjas 2 Plugins ########"
+echo "#########################################"
+echo
+cd
+sudo apt install -y libgl1-mesa-dev libx11-dev libsndfile1-dev libsamplerate0-dev
+echo
+git clone --recursive https://github.com/rghvdberg/ninjas2.git
+echo
+cd ninjas2
+echo
+make all CXXFLAGS='-mtune=native' CFLAGS='-mtune=native' CPPFLAGS='-mtune=native'
+echo
+sudo make install
+echo
+rm -rf ../ninjas2
+echo
+echo "Ninjas 2 Installed"
+echo
+cd
+echo
+echo "####################################################################################################################"
+echo "##### Getting Samplicity Samples and save to Music Folder I will make one folder if you are not  in English OS #####"
+echo "####################################################################################################################"
+mkdir Music
+wget -c https://archive.org/download/Samplicity/Samplicity_M7_Main-02-Wave32bit-48Khz_v1.1.zip
+unzip Sampli*.zip
+rm Sampli*.zip
+mv Samplicity_M7_Main-02-Wave32bit-48Khz_v1.1 Music/
+echo
+echo
+echo "Samples Downloaded"
+echo
+ARCH=32 #32-bit architecture
+echo "########################################"
+echo "##### Install Reaper DAW for armhf #####"
+echo "########################################"
+cd
+rm reaper*.tar.xz
+rm -rf reaper_linux_armv7l
+wget -c https://www.reaper.fm/files/6.x/reaper${REAPER}_linux_armv7l.tar.xz
+echo
+echo "Untar"
+echo
+tar -Jxvf reaper*.tar.xz
+echo
+cd reaper_linux_armv7l
+sudo ./install-reaper.sh --install /opt --integrate-desktop --usr-local-bin-symlink --quiet
+echo
+echo "REAPER Installed"
+echo
+rm ../reaper*.tar.xz
+rm -rf ../reaper_linux_armv7l
+echo
+echo "#####################################"
+echo "########## Install Plugins ##########"
+echo "#####################################"
+echo
+sudo apt install -y vitalium zynaddsubfx zynaddsubfx-dssi zynaddsubfx-lv2 zynaddsubfx-vst wah-plugins swh-lv2 swankyamp swankyamp-lv2 swankyamp-vst sorcer yoshimi carla carla-lv2 helm airwindows cardinal cardinal-lv2 cardinal-vst2 cardinal-vst3 phasex lmms petri-foo adlplug ams amsynth ardour drumkv1-lv2 fomp hydrogen padthv1-lv2 zam-plugins wolf-spectrum wolf-shaper teragonaudio-plugins temper shiro-plugins tal-plugins pizmidi-plugins pitcheddelay oxefmsynth obxd mda-lv2 luftikus lsp-plugins-vst lufsmeter linuxsampler-vst juced-plugins klangfalter juce-opl jackass hybridreverb2 easyssp drumgizmo drowaudio-plugins dpf-plugins distrho-plugin-ports dexed arctican-plugins vocproc tap-lv2 synthv1-lv2 zlfo so-synth-lv2 sherlock.lv2 samplv1-lv2 rubberband-lv2 noise-repellent moony.lv2 mod-pitchshifter mod-distortion melmatcheq.lv2 lv2vocoder kxstudio-recommended-audio-plugins-lv2 infamous-plugins gxvoxtonebender gxplugins gxplugins-data guitarix-lv2 guitarix-ladspa geonkick eq10q drmr caps-lv2 calf-plugins bshapr bsequencer blop-lv2 bjumblr beatslash-lv2 avldrums.lv2 abgate fabla wolpertinger
+cd
+echo
+echo "#################################"
+echo "########## LSP Plugins ##########"
+echo "#################################"
+echo
+echo "VST2"
+echo
+wget -c https://sourceforge.net/projects/lsp-plugins/files/lsp-plugins/${LSP}/Linux-armv7a/lsp-plugins-vst2-${LSP}-Linux-arm32.tar.gz
+echo
+tar -xzvf lsp-plugins-vst*.tar.gz
+cd lsp-plugins-vst2-${LSP}-Linux-arm32/
+sudo cp -r usr/* /usr
+echo
+echo "VST2 Installed"
+echo
+echo "LV2"
+echo
+cd
+wget -c https://sourceforge.net/projects/lsp-plugins/files/lsp-plugins/${LSP}/Linux-armv7a/lsp-plugins-lv2-${LSP}-Linux-arm32.tar.gz
+echo
+tar -xzvf lsp-plugins-lv2*.tar.gz
+cd lsp-plugins-lv2-${LSP}-Linux-arm32/
+sudo cp -r usr/* /usr
+echo
+echo "LV2 Installed"
+echo
+echo "JACK"
+echo
+cd
+wget -c https://sourceforge.net/projects/lsp-plugins/files/lsp-plugins/${LSP}}/Linux-armv7a/lsp-plugins-jack-${LSP}-Linux-arm32.tar.gz
+echo
+tar -xzvf lsp-plugins-jack*.tar.gz
+cd lsp-plugins-jack-${LSP}-Linux-arm32/
+sudo cp -r usr/* /usr
+echo
+echo "JACK Installed"
+echo
+echo "LADSPA"
+echo
+cd
+wget -c https://sourceforge.net/projects/lsp-plugins/files/lsp-plugins/${LSP}/Linux-armv7a/lsp-plugins-ladspa-${LSP}-Linux-arm32.tar.gz
+echo
+tar -xzvf lsp-plugins-ladspa*.tar.gz
+cd lsp-plugins-ladspa-${LSP}-Linux-arm32/
+sudo cp -r usr/* /usr
+echo
+echo "LADSPA Installed"
+echo
+echo "CLAP"
+echo
+cd
+wget -c https://sourceforge.net/projects/lsp-plugins/files/lsp-plugins/${LSP}/Linux-armv7a/lsp-plugins-clap-${LSP}-Linux-arm32.tar.gz
+echo
+tar -xzvf lsp-plugins-clap*.tar.gz
+cd lsp-plugins-clap-${LSP}-Linux-arm32/
+sudo cp -r usr/* /usr
+echo
+echo "CLAP Installed"
+echo
+cd
+echo
+echo "LSP Installed"
+echo
+rm -rf lsp*
+echo
+echo "X42 Plugins"
+echo
+cd
+wget -c http://x42-plugins.com/x42/linux/x42-compressor-v${X42}-armhf.tar.gz
+echo
+tar -xzvf x42*.tar.gz
+cd x42-compressor
+yes | ./install-lv2.sh
+rm -rf ../x42*
+cd
+echo
+echo "X42 Installed"
+echo
+echo "Compille and Install Stochas"
+echo
+cd
+sudo apt update
+sudo apt-get install -y git build-essential libgtk-3-dev libwebkit2gtk-4.0 libwebkit2gtk-4.0-dev libcurl4-openssl-dev alsa-tools libasound2-dev libjack-dev libfreetype6-dev libxinerama-dev libxcb-xinerama0 libxinerama1 x11proto-xinerama-dev libxrandr-dev libgl1-mesa-dev libxcursor-dev libxcursor1 libxcb-cursor-dev libxcb-cursor0
+echo
+git clone https://github.com/surge-synthesizer/stochas.git
+cd stochas/
+git submodule update --depth 1 --init --recursive
+export SVER=`cat VERSION`
+export GH=`git log -1 --format=%h`
+echo "Version ${SVER} hash ${GH}"
+cmake -Bbuild -DSTOCHAS_VERSION=${SVER}
+cmake --build build --config Release
+cp -rf $HOME/stochas/build/stochas_artefacts/VST3/Stochas.vst3 $HOME/.vst3
+rm -rf $HOME/stochas
+cd
+echo
+echo  "Ninjas 2 Standalone"
+echo
+sudo apt install -y libjack-dev
+echo
+echo "ReaPack"
+echo
+wget -c https://github.com/cfillion/reapack/releases/download/v${REAPACK}/reaper_reapack-armv7l.so
+echo
+cp reaper_reapack*.so $HOME/.config/REAPER/UserPlugins/
+echo
+echo "SWS S&M Extension"
+echo
+cd $HOME/.config/REAPER/
+wget -c https://sws-extension.org/download/featured/sws-2.12.1.3-Linux-armv7l.tar.xz
+echo
+tar -Jxvf sws*.tar.xz
+rm sws*
+cd
+echo
+echo "Adding PatchboxOS Repo"
+echo
+curl https://blokas.io/apt-setup.sh | sh
+sudo apt update
+echo
+sudo apt install pisound-ctl modep-ctl-scripts modep-btn-scripts modep-touchos2midi modep -y
+echo
+echo "Installing PureData, Mec, ORAC"
+echo
+sudo apt install puredata mec orac -y
+echo
+echo "Surge 1.8.1"
+echo
+cd
+sudo apt remove surge
+sudo apt autoremove -y
+git clone -b release/1.8.1 https://github.com/surge-synthesizer/surge
+cd surge
+git submodule update --init --recursive
+sudo apt-get update
+sudo apt-get install -y libgtk-3-dev
+sudo apt-get install -y libwebkit2gtk-4.0
+sudo apt-get install -y libwebkit2gtk-4.0-dev
+sudo apt-get install -y libcurl4-openssl-dev
+sudo apt-get install -y alsa
+sudo apt-get install -y alsa-tools
+sudo apt-get install -y libasound2-dev
+sudo apt-get install -y libjack-dev
+sudo apt-get install -y libfreetype6-dev
+sudo apt-get install -y libxinerama-dev
+sudo apt-get install -y libxcb-xinerama0
+sudo apt-get install -y libxinerama1
+sudo apt-get install -y x11proto-xinerama-dev
+sudo apt-get install -y libxrandr-dev
+sudo apt-get install -y libgl1-mesa-dev
+sudo apt-get install -y libxcursor-dev
+sudo apt-get install -y libxcursor1
+sudo apt-get install -y libxcb-cursor-dev
+sudo apt-get install -y libxcb-cursor0
+sudo apt-get install -y --fix-missing
+sudo apt install -y build-essential libcairo-dev libxkbcommon-x11-dev libxkbcommon-dev libxcb-cursor-dev libxcb-keysyms1-dev libxcb-util-dev libxrandr-dev libxinerama-dev libxcursor-dev libasound2-dev libjack-jackd2-dev
+cd
+cd surge
+./build-linux.sh build
+echo
+sudo ./build-linux.sh install
+rm -rf ../surge
+echo "Surge Installed"
+cd
+echo
+echo "Finished install on $ARCH bits OS, check tutorial for Tukan plugins"
+echo
+echo "Reboot the Pi"
