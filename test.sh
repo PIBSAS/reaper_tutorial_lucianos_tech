21dcd778700cae9358f45b9274566e55a12341ec
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2025-03-23 14:11:48 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -55,8 +55,8 @@ cd surge
 git submodule update --init --recursive
 cmake -Bignore/s13clang -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
 cmake --build ignore/s13clang --target all --parallel $(nproc)
-sudo cmake --install ignore/s13clang
-rm -rf "$HOME/surge"
+#sudo cmake --install ignore/s13clang
+#rm -rf "$HOME/surge"
 titulo "Surge XT Plugins installed"
 titulo "Finished install on $(uname -m) bits OS, check tutorial for Tukan plugins"
 titulo "Reverting sudoers temp file, please make sure reboot..."
