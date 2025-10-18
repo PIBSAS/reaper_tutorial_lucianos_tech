aa5bfb2fdac911270a619b936e83029ebc44fc78
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2025-10-18 10:15:43 -0300

    Enhance REAPER setup with ReaPack and KXStudio
    Added ReaPack pluginplus repos so you can go to extensions anc choose between >5k packages to install

--- reaper_Buster_Bullseye_64bit.sh
+++ reaper_Buster_Bullseye_64bit.sh
@@ -53,6 +53,15 @@ titulo "Adding Plugins Paths"
 sed -i 's!vstpath=!vstpath=$HOME/.vst;$HOME/.vst3;/lib/vst;/lib/vst3;/lib/lxvst;/usr/lib/vst;/usr/lib/vst3;/usr/lib/lxvst;/usr/local/lib/vst;/usr/local/lib/vst3;/usr/lib/lxvst;/lib/vst/carla.vst/;!' $HOME/.config/REAPER/reaper.ini
 sed -i 's!lv2path_linux=!lv2path_linux=/usr/lib/lv2;/usr/local/lib/lv2;$HOME/.lv2;/lib/lv2;/usr/modep/lv2;!' $HOME/.config/REAPER/reaper.ini
 sed -i '/^fxdenorm=1.*/i clap_path_linux-aarch64=/usr/local/lib/clap;/usr/lib/clap;$HOME/.clap;%CLAP_PATH%;/lib/clap;/usr/local/lib/clap;' $HOME/.config/REAPER/reaper.ini
+titulo "ReaPack"
+cd
+url="https://github.com/cfillion/reapack/releases/latest/" ; \
+latest_version=$(curl -sSL ${url} | grep -o 'tag/v[0-9.]*' | head -n 1 | cut -d '/' -f 2 | sed 's/^v//') ; \
+wget -c "https://github.com/cfillion/reapack/releases/download/v${latest_version}/reaper_reapack-aarch64.so"
+cp reaper_reapack*.so $HOME/.config/REAPER/UserPlugins/
+rm reaper_reapack*.so
+titulo "ReaPack Repositories Setup from Luciano's Tech"
+bash <(curl -fsSL https://raw.githubusercontent.com/PIBSAS/reaper_tutorial_lucianos_tech/main/repos_reapack.sh)
 titulo "Adding KXStudio Repository to the OS"
 cd
 url="https://kx.studio/Repositories" ; \
